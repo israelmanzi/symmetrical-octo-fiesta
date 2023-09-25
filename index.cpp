44e143e7d49f40263c8a90045d998644d67ddf40
israel_manzi <104481099+israelmanzi@users.noreply.github.com>
2023-09-25 11:55:33 +0200

    [fx]: cpp impl optimization

--- index.cpp
+++ index.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include <sstream>
 
 class CException: public std::exception
 {
@@ -23,70 +24,37 @@ typedef std::pair<std::string, std::string> Query;
 std::string trim_input(const std::string& _string_to_trim)
 {
     // Position of the first non-whitespace character
-	size_t start = _string_to_trim.find_first_not_of(" \t\n\r");
+    std::string_view string_view(_string_to_trim);
+	size_t start = string_view.find_first_not_of(" \t\n\r");
 
     // If there is no non-whitespace character throw an exception
-	if (start == std::string::npos) {
+	if (start == std::string_view::npos) {
         throw CException("Invalid string!");
 	}
 
     // Position of the last non-whitespace character
     size_t end = _string_to_trim.find_last_not_of(" \t\n\r");
-    size_t length = end - start + 1; // Length of the string without whitespace characters
 
     // Trim the string from the starting position for a specified length
-    return _string_to_trim.substr(start, length);
+    return _string_to_trim.substr(start, end - start + 1);
 }
 
 std::vector<Query> split_string(const std::string& _string_to_split)
 {
-    // Trim the string from whitespace characters
-    const std::string& trimmed_string = trim_input(_string_to_split);
-    const std::string& delimiter = "&";
-
-    // If the string is empty return an empty vector
-    size_t start = 0, end, delimiter_length = delimiter.length();
+    std::istringstream ss(trim_input(_string_to_split));
     std::string token;
-    std::vector<std::string> tokens;
     std::vector<Query> queries;
 
-    // Split the string by the delimiter
-    while ((end = trimmed_string.find(delimiter, start)) != std::string::npos) {
-        token = trimmed_string.substr(start, end - start);
+    while (std::getline(ss, token, '&')) {
+        std::istringstream token_ss(token);
+        std::string key, value;
 
-        // If the token is empty throw an exception
-        if (token.empty()) {
+        if (std::getline(token_ss, key, '=') && std::getline(token_ss, value)) {
+            Query query(key, value);
+            queries.emplace_back(query);
+        } else {
             throw CException("Invalid string!");
         }
-
-        tokens.push_back(token);
-        start = end + delimiter_length;
-    }
-
-    // Add the last token
-    token = trimmed_string.substr(start);
-    tokens.push_back(token);
-
-    for (const std::string& _token : tokens) {
-        size_t pos = _token.find("=");
-
-        // If the token is empty throw an exception
-        if (pos == std::string::npos) {
-            throw CException("Invalid string!");
-        }
-
-        // Split the token by the "=" sign and get the key and the value
-        std::string key = _token.substr(0, pos);
-        std::string value = _token.substr(pos + 1);
-
-        // If the key or the value is empty throw an exception
-        if (key.empty() || value.empty()) {
-            throw CException("Invalid string!");
-        }
-
-        // Add the key-value pair to the vector
-        Query query(key, value);
-        queries.push_back(query);
     }
 
     return queries;
